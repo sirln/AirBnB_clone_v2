6006cefd3f0338ec8e6a91456d1f779fd16f9d7a
amisoni70 <amisoni7014@gmail.com>
2023-09-19 12:34:19 -0700

    state.py task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ The class State representation of MySql Database.
+
+    Inherits from SQLAlchemy Base and links to the MySql table states.
+
+    Attributes:
+    __tablename__(str): Table name to store states
+    name(str): The name of the state.
+    """
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref= "state", cascade= "delete")
