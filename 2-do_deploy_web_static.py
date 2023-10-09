1106adcab399a5e4eb8c4c37b5296912589fd8ac
sirln <siro@sirlawren.com>
2023-10-09 11:26:35 +0300

    AirBnB clone - Deploy static. Script to deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,19 +31,19 @@ def do_deploy(archive_path):
         release_dir = f'/data/web_static/releases/{no_ext_name}'
 
         # Uncompress the archive to the folder on the web server
-        run(f'mkdir -p {release_dir}')
-        run(f'tar -xzf /tmp/{archive_name} -C {release_dir}')
+#        run(f'mkdir -p {release_dir}')
+#        run(f'tar -xzf /tmp/{archive_name} -C {release_dir}')
 
         # Delete the archive from the web server
-        run(f'rm /tmp/{archive_name}')
-        run(f'mv {release_dir}/web_static/* {release_dir}')
-        run(f'rm -rf {release_dir}/web_static')
+#        run(f'rm /tmp/{archive_name}')
+#        run(f'mv {release_dir}/web_static/* {release_dir}')
+#        run(f'rm -rf {release_dir}/web_static')
 
         # Delete the symbolic link /data/web_static/current from the web server
-        run('rm -rf /data/web_static/current')
+#        run('rm -rf /data/web_static/current')
 
         # Create a new the symbolic link on the web server
-        run(f'ln -s {release_dir} /data/web_static/current')
+#        run(f'ln -s {release_dir} /data/web_static/current')
 
         return True
     except Exception:
